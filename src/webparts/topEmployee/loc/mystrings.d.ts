62034e2a257f26319670d7f54b9a10390a0e9306
abashutosh <abashutosh@gmail.com>
2019-05-27 12:34:09 +0000

    Added spfx solution to folder

---
+++ src/webparts/topEmployee/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITopEmployeeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TopEmployeeWebPartStrings' {
+  const strings: ITopEmployeeWebPartStrings;
+  export = strings;
+}
